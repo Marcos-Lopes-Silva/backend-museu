48c4b879a9ed0045d313232e25ad63da2008d181
Lucas Vieira <lucasvdc.aluno@unipampa.edu.br>
2023-10-10 03:17:39 -0300

    cadastrar funcionarios

--- frontend/src/pages/funcionario/listFuncionario/rendererListFuncionario.ts
+++ frontend/src/pages/funcionario/listFuncionario/rendererListFuncionario.ts
@@ -1,1 +1,22 @@
-const paginitial = document.querySelector('#paginitial');
+// getData('http://localhost:8080/funcionarios')
+//   .then((data) => {
+//     console.log(data.content);
+//     data.content.array.forEach(funcionario => {
+
+//     });
+//   })
+//   .catch((error) => {
+//     console.error('Erro:', error);
+//   });
+
+// async function getData(url = '') {
+//   const response = await fetch(url, {
+//     method: 'GET', // Altere para GET
+//     headers: {
+//       Authorization: `Bearer ${localStorage.getItem('token')}`,
+//       'Content-Type': 'application/json',
+//     },
+//     // Não inclua o corpo em uma solicitação GET
+//   });
+//   return response.json();
+// }
