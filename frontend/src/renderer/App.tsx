2b46364933bf8957c2590d4c25e0bec6e3e73f9b
Lucas Vieira <lucasvdc.aluno@unipampa.edu.br>
2023-10-31 17:02:09 -0300

    Add Cadastrar Divisao

--- frontend/src/renderer/App.tsx
+++ frontend/src/renderer/App.tsx
@@ -12,13 +12,16 @@ import Home from './pages/home/Home';
 import ListarFuncionario from './pages/funcionario/funcionario.listar';
 import EditarFuncionario from './pages/funcionario/funcionario.editar';
 import CadastrarFuncionario from './pages/funcionario/funcionario.cadastrar';
+import CadastrarDivisao from './pages/divisao/divisao.cadastrar';
+import ListarDivisao from './pages/divisao/divisao.listar';
+import CadastrarSecao from './pages/secao/secao.cadastrar';
 
 export default function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<ListarFuncionario />} />
+          <Route path="/" element={<CadastrarSecao />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/funcionarios" element={<ListarFuncionario />} />
@@ -30,6 +33,10 @@ export default function App() {
             path="/funcionarios/:id/editar"
             element={<EditarFuncionario />}
           />
+          <Route path="/divisao" element={<ListarDivisao />} />
+          <Route path="/divisao/cadastrar" element={<CadastrarDivisao />} />
+          <Route path="/secao" element={<ListarDivisao />} />
+          <Route path="/secao/cadastrar" element={<CadastrarSecao />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </Router>
